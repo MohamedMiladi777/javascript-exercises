a52c9268ac8f75c1a3cbbd2635dfa79cc3aedb93
MohamedMiladi777 <miladyshady@gmail.com>
2025-07-31 10:33:34 +0100

    Solved all problems

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let currentYear = new Date().getFullYear();
 
+  people.forEach((person) => {
+    if (!person.yearOfDeath) {
+      person.yearOfDeath = currentYear;
+    }
+  });
+  
+  let results = [];
+  let calculateAge = 0;
+  people.forEach((person) => {
+    calculateAge = person.yearOfDeath - person.yearOfBirth;
+    results.push({ calculateAge, person: person });
+  });
+  let oldest = results[0];
+  for (let i = 1; i < results.length; i++) {
+    if (results[i].calculateAge > oldest.calculateAge) {
+      oldest = results[i];
+    }
+  }
+
+  return oldest.person;
 };
 
 // Do not edit below this line
